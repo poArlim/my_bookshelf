d3076a3dcb39aafc9fa973698a931c11fd759fd9
poArlim <dkfhscjsgk96@gmail.com>
2020-10-27 17:03:51 +0900

    ..

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,14 +3,14 @@ module.exports = function(app){
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://localhost:5000',
+            target: 'http://192.168.0.14:5000',
             changeOrigin: true,
         })
     );
     app.use(
         '/uploads',
         createProxyMiddleware({
-            target: 'http://localhost:5000',
+            target: 'http://192.168.0.14:5000',
             changeOrigin: true,
         })
     );
